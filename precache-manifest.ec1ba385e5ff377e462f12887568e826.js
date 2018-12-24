4b691587f44b4ef766cc9b291fa3c409dfe194d6
MikeyZat <mikeyzat@gmail.com>
2018-12-24 13:38:18 +0100

    Updates

---
+++ precache-manifest.ec1ba385e5ff377e462f12887568e826.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c46df3352c34cc4b1f7",
+    "url": "/AirCondition/static/css/main.6df2a615.chunk.css"
+  },
+  {
+    "revision": "1c46df3352c34cc4b1f7",
+    "url": "/AirCondition/static/js/main.1c46df33.chunk.js"
+  },
+  {
+    "revision": "992eeab57fe488dbaf35",
+    "url": "/AirCondition/static/js/1.992eeab5.chunk.js"
+  },
+  {
+    "revision": "173b42274e170b40e415",
+    "url": "/AirCondition/static/js/runtime~main.173b4227.js"
+  },
+  {
+    "revision": "ce7cefbe826a2ad1fd891b487eb07b11",
+    "url": "/AirCondition/index.html"
+  }
+];
